c8f64648fb5558d2ab3ff8116dd9e931465f26e0
Ivan Kosaev <119807994+PAGUR0@users.noreply.github.com>
2024-04-15 15:11:13 +0300

    Добавьте файлы проекта.

---
+++ PP_Lab4_C#/Colledge.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace colledge
+{
+    struct Student
+    {
+        public string Surname;
+        public string Name;
+        public string Patronymic;
+        public string GroupNumber;
+        private int Age;
+
+        public Student(string surname, string name, string patronymic, string groupNumber, int age)
+        {
+            this.Surname = surname;
+            this.Name = name;
+            this.Patronymic = patronymic;
+            this.GroupNumber = groupNumber;
+            this.Age = age;
+        }
+
+        public void infoStudent()
+        {
+            Console.WriteLine($"{Surname}, {Name}, {Patronymic}, {GroupNumber},{Age}");
+        }
+    }
+}
---
+++ PP_Lab4_C#/Library.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class Reader
+    {
+        private string Surname;
+        private string Name;
+        private string Patronymic;
+        public int Number;
+        public string Faculty;
+        private string Birthday;
+        public string Phone;
+
+        public Reader(string surname, string name, string patronymic, int number, string faculty, string birhday, string phone)
+        {
+            this.Surname = surname;
+            this.Name = name;
+            this.Patronymic = patronymic;
+            this.Number = number;
+            this.Faculty = faculty;
+            this.Birthday = birhday;
+            this.Phone = phone;
+        }
+
+        public void takeBook(int books)
+        {
+            Console.WriteLine($"{Surname}, {Name}, {Patronymic} взял {books} книги");
+        }
+        public void takeBook(string[] books)
+        {
+            Console.WriteLine($"{Surname}, {Name}, {Patronymic} взял книги: {string.Join(", ", books)}");
+        }
+
+        public void returnBook(int books)
+        {
+            Console.WriteLine($"{Surname}, {Name}, {Patronymic} вернул {books} книги");
+        }
+        public void returnBook(string[] books)
+        {
+            Console.WriteLine($"{Surname}, {Name}, {Patronymic} вернул книги: {string.Join(", ", books)}");
+        }
+    }
+}
